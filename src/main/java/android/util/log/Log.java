18b4f3b3506cdf79848f08d590a6f333c49ffb18
Yuri Pourre <yuriamayuri@gmail.com>
2015-06-12 10:59:52 -0400

    Add Log.e method

--- src/main/java/android/util/log/Log.java
+++ src/main/java/android/util/log/Log.java
@@ -5,5 +5,9 @@ public class Log {
 	public static void d(String tag, String text) {
 		System.out.println(tag+": "+text);
 	}
+	
+	public static void e(String tag, String text) {
+		System.err.println(tag+": "+text);
+	}
 
 }
