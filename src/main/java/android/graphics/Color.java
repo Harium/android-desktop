efece46b8a46a92a1662244f8c1654587538e5db
Yuri Pourre <yuriamayuri@gmail.com>
2015-08-07 18:44:48 -0300

    Add Android color stub

---
+++ src/main/java/android/graphics/Color.java
@@ -0,0 +1,10 @@
+package android.graphics;
+
+public class Color {
+
+	public static final int BLACK = java.awt.Color.BLACK.getRGB();
+	public static final int BLUE = java.awt.Color.BLUE.getRGB();
+	public static final int RED = java.awt.Color.RED.getRGB();
+	public static final int WHITE = java.awt.Color.WHITE.getRGB();
+	
+}
