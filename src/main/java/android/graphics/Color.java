ecc17cf6664d80b98be64ec1a82f61bf14c3b4d2
Yuri Pourre <yuriamayuri@gmail.com>
2017-05-17 17:04:25 -0300

    Update Color

--- src/main/java/android/graphics/Color.java
+++ src/main/java/android/graphics/Color.java
@@ -2,9 +2,105 @@ package android.graphics;
 
 public class Color {
 
-	public static final int BLACK = java.awt.Color.BLACK.getRGB();
-	public static final int BLUE = java.awt.Color.BLUE.getRGB();
-	public static final int RED = java.awt.Color.RED.getRGB();
-	public static final int WHITE = java.awt.Color.WHITE.getRGB();
-	
+    public static final int BLACK = java.awt.Color.BLACK.getRGB();
+    public static final int DKGRAY = java.awt.Color.DARK_GRAY.getRGB();
+    public static final int GRAY = java.awt.Color.GRAY.getRGB();
+    public static final int LTGRAY = java.awt.Color.LIGHT_GRAY.getRGB();
+    public static final int GREEN = java.awt.Color.GREEN.getRGB();
+    public static final int BLUE = java.awt.Color.BLUE.getRGB();
+    public static final int CYAN = java.awt.Color.CYAN.getRGB();
+    public static final int RED = java.awt.Color.RED.getRGB();
+    public static final int MAGENTA = java.awt.Color.MAGENTA.getRGB();
+    public static final int YELLOW = java.awt.Color.YELLOW.getRGB();
+    public static final int WHITE = java.awt.Color.WHITE.getRGB();
+    public static final int TRANSPARENT = 0;
+
+    /**
+     * Return the alpha component of a color int. This is the same as saying
+     * color >>> 24
+     */
+    public static int alpha(int color) {
+        return color >>> 24;
+    }
+
+    /**
+     * Return the red component of a color int. This is the same as saying
+     * (color >> 16) & 0xFF
+     */
+    public static int red(int color) {
+        return (color >> 16) & 0xFF;
+    }
+
+    /**
+     * Return the green component of a color int. This is the same as saying
+     * (color >> 8) & 0xFF
+     */
+    public static int green(int color) {
+        return (color >> 8) & 0xFF;
+    }
+
+    /**
+     * Return the blue component of a color int. This is the same as saying
+     * color & 0xFF
+     */
+    public static int blue(int color) {
+        return color & 0xFF;
+    }
+
+    /**
+     * Return a color-int from red, green, blue components.
+     * The alpha component is implicity 255 (fully opaque).
+     * These component values should be [0..255], but there is no
+     * range check performed, so if they are out of range, the
+     * returned color is undefined.
+     *
+     * @param red   Red component [0..255] of the color
+     * @param green Green component [0..255] of the color
+     * @param blue  Blue component [0..255] of the color
+     */
+    public static int rgb(int red, int green, int blue) {
+        return (0xFF << 24) | (red << 16) | (green << 8) | blue;
+    }
+
+    /**
+     * Return a color-int from alpha, red, green, blue components.
+     * These component values should be [0..255], but there is no
+     * range check performed, so if they are out of range, the
+     * returned color is undefined.
+     *
+     * @param alpha Alpha component [0..255] of the color
+     * @param red   Red component [0..255] of the color
+     * @param green Green component [0..255] of the color
+     * @param blue  Blue component [0..255] of the color
+     */
+    public static int argb(int alpha, int red, int green, int blue) {
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
+
+    /**
+     * Parse the color string, and return the corresponding color-int.
+     * If the string cannot be parsed, throws an IllegalArgumentException
+     * exception. Supported formats are:
+     * #RRGGBB
+     * #AARRGGBB
+     * 'red', 'blue', 'green', 'black', 'white', 'gray', 'cyan', 'magenta',
+     * 'yellow', 'lightgray', 'darkgray', 'grey', 'lightgrey', 'darkgrey',
+     * 'aqua', 'fuschia', 'lime', 'maroon', 'navy', 'olive', 'purple',
+     * 'silver', 'teal'
+     */
+    public static int parseColor(String colorString) {
+        if (colorString.charAt(0) == '#') {
+            // Use a long to avoid rollovers on #ffXXXXXX
+            long color = Long.parseLong(colorString.substring(1), 16);
+            if (colorString.length() == 7) {
+                // Set the alpha value
+                color |= 0x00000000ff000000;
+            } else if (colorString.length() != 9) {
+                throw new IllegalArgumentException("Unknown color");
+            }
+            return (int) color;
+        }
+        throw new IllegalArgumentException("Unknown color");
+    }
+
 }
